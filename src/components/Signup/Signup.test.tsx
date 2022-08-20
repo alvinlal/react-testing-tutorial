092aebaf855255f49936eaff1170291657d8a5ee
alvinlal <alvinzzz2001@gmail.com>
2022-08-20 23:04:15 +0530

    user events

--- src/components/Signup/Signup.test.tsx
+++ src/components/Signup/Signup.test.tsx
@@ -1,8 +1,16 @@
 /*
  * 1. All inputs in the signup form should be initially empty
+ * 2. Should be able to type an email
+ * 3. Should be able to type a password
+ * 4. Should be able to type a confirm password
+ * 5. Should show email error message on invalid email
+ * 6. Should show email error message only if both email and password is invalid
+ * 7. Should show password error if password length is less than 5 characters
+ * 8. Should show confirm password error if password and confirm password fields don't match
  */
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Signup from './Signup';
 
 describe('Signup.tsx', () => {
@@ -17,5 +25,49 @@ describe('Signup.tsx', () => {
     expect(passwordInputElement.value).toBe('');
     expect(emailInputElement.value).toBe('');
     expect(confirmPasswordInputElement.value).toBe('');
+  });
+
+  it('should be able to type an email', () => {
+    render(<Signup />);
+    const emailInputElement = screen.getByRole('textbox', {
+      name: /email/i,
+    }) as HTMLInputElement;
+    userEvent.type(emailInputElement, 'alvin@gmail.com');
+    expect(emailInputElement.value).toBe('alvin@gmail.com');
+  });
+
+  it('should be able to type a password', () => {
+    render(<Signup />);
+    const passwordInputElement = screen.getByLabelText('Password') as HTMLInputElement;
+    userEvent.type(passwordInputElement, 'test123');
+    expect(passwordInputElement.value).toBe('test123');
+  });
+  it('should be able to type a confirm password', () => {
+    render(<Signup />);
+    const confirmPasswordInputElement = screen.getByLabelText(
+      'Confirm Password'
+    ) as HTMLInputElement;
+    userEvent.type(confirmPasswordInputElement, 'test123');
+    expect(confirmPasswordInputElement.value).toBe('test123');
+  });
+  it('should show invalid email error message on invalid email', () => {
+    render(<Signup />);
+
+    let emailErrorElement = screen.queryByText(/the email you input is invalid/i);
+    const emailInputElement = screen.getByRole('textbox', {
+      name: /email/i,
+    });
+    const submitBtnElement = screen.getByRole('button', {
+      name: /submit/i,
+    });
+
+    expect(emailErrorElement).not.toBeInTheDocument();
+
+    userEvent.type(emailInputElement, 'alvingmail.com');
+    userEvent.click(submitBtnElement);
+
+    emailErrorElement = screen.queryByText(/the email you input is invalid/i);
+
+    expect(emailErrorElement).toBeInTheDocument();
   });
 });
--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,4 +1,34 @@
+import { ChangeEvent, useState, MouseEvent } from 'react';
+import validator from 'validator';
+interface SignupInput {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 const Signup: React.FC = () => {
+  const [signupInput, setSignInput] = useState<SignupInput>({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [error, setError] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSignInput({
+      ...signupInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = (e: MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    if (!validator.isEmail(signupInput.email)) {
+      setError('the email you input is invalid');
+    }
+  };
+
   return (
     <div className='container my-5'>
       <form>
@@ -6,13 +36,27 @@ const Signup: React.FC = () => {
           <label htmlFor='email' className='form-label'>
             Email Address
           </label>
-          <input type='email' id='email' name='email' className='form-control' />
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='form-control'
+            value={signupInput.email}
+            onChange={handleChange}
+          />
         </div>
         <div className='mb-3'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
-          <input type='password' id='password' name='password' className='form-control' />
+          <input
+            type='password'
+            id='password'
+            name='password'
+            className='form-control'
+            value={signupInput.password}
+            onChange={handleChange}
+          />
         </div>
         <div className='mb-3'>
           <label htmlFor='confirm-password' className='form-label'>
@@ -21,10 +65,16 @@ const Signup: React.FC = () => {
           <input
             type='password'
             id='confirm-password'
-            name='confirm-password'
+            name='confirmPassword'
             className='form-control'
+            value={signupInput.confirmPassword}
+            onChange={handleChange}
           />
         </div>
+        {error && <p className='text-danger'>{error}</p>}
+        <button type='submit' name='submit' className='btn btn-primary' onClick={handleClick}>
+          Submit
+        </button>
       </form>
     </div>
   );
